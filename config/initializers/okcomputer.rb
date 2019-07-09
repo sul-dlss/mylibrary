9594bf2180c96da5847f5619e0643951a175e360
Chris Beer <cabeer@stanford.edu>
2019-07-09 07:47:47 -0700

    Poke the SymphonyClient ping handler through okcomputer

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -1,3 +1,14 @@
 # frozen_string_literal: true
 
 OkComputer.mount_at = false
+
+# OKComputer check that checks if we have a connection to symws
+class SymphonyClientCheck < OkComputer::Check
+  def check
+    ping = SymphonyClient.new.ping
+
+    mark_failure unless ping
+  end
+end
+
+OkComputer::Registry.register 'symphony_web_services', SymphonyClientCheck.new
