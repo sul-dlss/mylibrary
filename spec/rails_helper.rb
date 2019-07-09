dea9b58850e60c9d46b3acb1e2b7543e8dc7f01f
Jack Reed <phillipjreed@gmail.com>
2019-07-09 15:06:14 -0600

    Set mylibrary to explicitly use Selenium Chrome Headless to run tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,11 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/rspec'
+
+require 'selenium-webdriver'
+
+Capybara.javascript_driver = :selenium_chrome_headless
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
