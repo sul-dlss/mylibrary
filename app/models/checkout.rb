0c34a8620d9747ed8193b59ab1eab35201fb494c
Shelley Doljack <sdoljack@stanford.edu>
2019-07-10 15:01:41 -0700

    Extracts a checkout model.

---
+++ app/models/checkout.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+# Model for the Checkouts page
+class Checkout
+  attr_reader :record
+
+  def initialize(record)
+    @record = record
+  end
+
+  def key
+    record['key']
+  end
+
+  def status
+    fields['status']
+  end
+
+  def due_date
+    Time.zone.parse(fields['dueDate'])
+  end
+
+  def checkout_date
+    Time.zone.parse(fields['checkOutDate'])
+  end
+
+  def recalled_date
+    Time.zone.parse(fields['recalledDate']) if fields['recalledDate']
+  end
+
+  def renewal_date
+    Time.zone.parse(fields['renewalDate']) if fields['renewalDate']
+  end
+
+  def overdue?
+    fields['overdue']
+  end
+
+  def library
+    fields['library']['key']
+  end
+
+  def title
+    bib['title']
+  end
+
+  def author
+    bib['author']
+  end
+
+  def call_number
+    call['dispCallNumber']
+  end
+
+  def shelf_key
+    call['sortCallNumber']
+  end
+
+  private
+
+  def fields
+    record['fields']
+  end
+
+  def bib
+    fields['item']['fields']['bib']['fields']
+  end
+
+  def call
+    fields['item']['fields']['call']['fields']
+  end
+end
---
+++ spec/models/checkout_spec.rb
@@ -0,0 +1,109 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Checkout do
+  subject do
+    described_class.new({
+      key: '1',
+      fields: fields
+    }.with_indifferent_access)
+  end
+
+  let(:checkout) { subject }
+  let(:fields) do
+    {
+      status: 'ACTIVE',
+      overdue: true,
+      checkOutDate: '2019-07-08T21:28:00-07:00',
+      dueDate: '2019-07-09T23:59:00-07:00',
+      library: {
+        key: 'LAW'
+      },
+      item: {
+        fields: {
+          bib: {
+            fields: {
+              title: 'The Lego movie videogame [electronic resource]',
+              author: 'Cool people made this'
+            }
+          },
+          call: {
+            fields: {
+              dispCallNumber: 'ZMS 4033',
+              sortCallNumber: 'ZMS 004033'
+            }
+          }
+        }
+      }
+    }
+  end
+
+  it 'has a key' do
+    expect(checkout.key).to eq '1'
+  end
+
+  it 'has a status' do
+    expect(checkout.status).to eq 'ACTIVE'
+  end
+
+  it 'has an overdue state' do
+    expect(checkout.overdue?).to be true
+  end
+
+  it 'has a due date' do
+    expect(checkout.due_date.strftime('%m/%d/%Y')).to eq '07/09/2019'
+  end
+
+  it 'has a checkout date' do
+    expect(checkout.checkout_date.strftime('%m/%d/%Y')).to eq '07/08/2019'
+  end
+
+  it 'does not have a renewal date' do
+    expect(checkout.renewal_date).to be_nil
+  end
+
+  context 'with a record that has been renewed' do
+    before do
+      fields[:renewalDate] = '2019-07-10T13:59:00-07:00'
+    end
+
+    it 'has a renewal date' do
+      expect(checkout.renewal_date.strftime('%m/%d/%Y')).to eq '07/10/2019'
+    end
+  end
+
+  it 'does not have a recalled date' do
+    expect(checkout.recalled_date).to be_nil
+  end
+
+  context 'with a record that has been recalled' do
+    before do
+      fields[:recalledDate] = '2019-07-11T13:59:00-07:00'
+    end
+
+    it 'has a recalled date' do
+      expect(checkout.recalled_date.strftime('%m/%d/%Y')).to eq '07/11/2019'
+    end
+  end
+
+  it 'has a library' do
+    expect(checkout.library).to eq 'LAW'
+  end
+
+  it 'has a title' do
+    expect(checkout.title).to eq 'The Lego movie videogame [electronic resource]'
+  end
+
+  it 'has an author' do
+    expect(checkout.author).to eq 'Cool people made this'
+  end
+
+  it 'has a call number' do
+    expect(checkout.call_number).to eq 'ZMS 4033'
+  end
+
+  it 'has a shelf key' do
+    expect(checkout.shelf_key).to eq 'ZMS 004033'
+  end
+end
