51662e1d35f8b99aaf4967e72ad1c3c68e1308c4
Chris Beer <cabeer@stanford.edu>
2019-07-11 15:22:59 -0700

    Be nice about unknown attributes for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,11 @@ class User
   include ActiveModel::Model
   attr_accessor :username, :patron_key, :shibboleth
 
+  # FIXME: This is temporary code and can be removed after everyone who has logged into -dev
+  # has logged out and logged in.
+  def initialize(attributes)
+    super(attributes.with_indifferent_access.slice(:username, :patron_key, :shibboleth))
+  end
+
   alias shibboleth? shibboleth
 end
