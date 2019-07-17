1807d20f58281d59c2c9bf0c84e6938b1138f230
Jack Reed <phillipjreed@gmail.com>
2019-07-17 12:56:29 -0600

    Add show/hide PIN functionality

--- spec/support/fake_symphony.rb
+++ spec/support/fake_symphony.rb
@@ -16,6 +16,12 @@ class FakeSymphony < Sinatra::Base
     {}.to_json
   end
 
+  post '/symws/user/patron/changeMyPin' do
+    content_type :json
+    status 200
+    {}.to_json
+  end
+
   get '/symws/user/patron/key/:key' do
     json_response 200, "patron/#{params[:key]}.json"
   end
