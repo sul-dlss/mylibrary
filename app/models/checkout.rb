cd0468352c77b6d3f149a529153e9a591cc76747
Jack Reed <mejackreed@users.noreply.github.com>
2019-07-25 13:07:56 -0600

    Update app/models/checkout.rb
    Co-Authored-By: Chris Beer <cabeer@stanford.edu>

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -55,7 +55,7 @@ class Checkout
   ##
   # Is this item renewable
   def renewable?
-    unseen_renewals_remaining.positive? && Time.zone.now > renewable_at if renewable_at
+    unseen_renewals_remaining.positive? && renewable_at.past? if renewable_at
   end
 
   def unseen_renewals_remaining
