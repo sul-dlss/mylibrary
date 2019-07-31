6b8270965c1bd5dfb6c4e3272bb7b0578b0a9053
Shelley Doljack <sdoljack@stanford.edu>
2019-07-30 17:02:39 -0700

    Adds Oracle DB connection class to get group circrecord keys.

--- app/services/symphony_db_client.rb
+++ app/services/symphony_db_client.rb
@@ -6,6 +6,7 @@ class SymphonyDbClient
 
   def initialize(patron_key)
     @patron_key = patron_key.to_i
+    @group_circrecord_keys = []
   end
 
   def connection
@@ -16,43 +17,23 @@ class SymphonyDbClient
     @connection.logoff
   end
 
-  def checkouts_query
-    'select catalog_key, call_sequence, copy_number, charge_number from charge' \
-    ' where user_key = :user_key and usergroup_key >=0'
+  def statement
+    'select catalog_key, call_sequence, copy_number, charge_number from charge where user_key = :user_key and usergroup_key >=0'
   end
 
   def cursor
     @cursor ||= begin
-      c = connection.parse(checkouts_query)
+      c = connection.parse(statement)
       c.bind_param(':user_key', patron_key, Integer)
     end
   end
 
   def group_circrecord_keys
-    @group_circrecord_keys ||= begin
-      cursor.exec
+    return @group_circrecord_keys if @group_circrecord_keys.any?
 
-      cursor.enum_for(:fetch).map { |row| row.join(':') }
-    end
-  end
-
-  def requests_query
-    'select key from hold where user_key = :user_key and usergroup_key >=0'
-  end
-
-  def requests_cursor
-    @requests_cursor ||= begin
-      c = connection.parse(requests_query)
-      c.bind_param(':user_key', patron_key, Integer)
-    end
-  end
-
-  def group_holdrecord_keys
-    @group_holdrecord_keys ||= begin
-      requests_cursor.exec
-
-      requests_cursor.enum_for(:fetch).map { |row| row.join('') }
-    end
+    cursor.exec
+    cursor.fetch { |row| @group_circrecord_keys << row.join(':') }
+    @group_circrecord_keys
   end
 
   private
