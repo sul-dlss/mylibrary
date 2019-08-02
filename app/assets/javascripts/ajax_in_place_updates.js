130d17adcf3bbe8badaed6e48c58af07ffd38f2b
Jessie Keck <jessie.keck@gmail.com>
2019-08-02 12:32:37 -0700

    Add initial in place update implementation

---
+++ app/assets/javascripts/ajax_in_place_updates.js
@@ -0,0 +1,9 @@
+$(document).on('turbolinks:load', function(){
+  $(document.body).on('ajax:success', function(event) {
+    var data = event.detail && event.detail[0];
+
+    if (data && data.key && data.type && data.html) {
+      $('[data-key="' + data.key + '"][data-type="' + data.type + '"]').replaceWith(data.html);
+    }
+  });
+});
