710a07a23c8320f6f8744e1133aff57bdf00ba65
Shelley Doljack <sdoljack@stanford.edu>
2019-08-02 16:27:28 -0700

    Removes pay button from group tab for both sponsor and proxy patrons.

---
+++ spec/features/sponsor_user_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Sponsor User', type: :feature do
+  before do
+    login_as(username: 'FACULTY2', patron_key: '521187')
+  end
+
+  it 'has a tab to switch between user and group' do
+    visit summaries_path
+
+    expect(page).to have_css('.nav-tabs .nav-link', count: 2)
+    expect(page).to have_css('.nav-tabs .nav-link.active', text: 'Self')
+
+    click_link 'Proxies'
+    expect(page).to have_css('.nav-tabs .nav-link.active', text: 'Proxies')
+
+    click_link 'Self'
+    expect(page).to have_css('.nav-tabs .nav-link.active', text: 'Self')
+  end
+
+  it 'shows the pay fine button on the self tab only' do
+    visit summaries_path
+
+    expect(page).to have_css('a.btn', text: 'Pay $13.00 now')
+
+    click_link 'Proxies'
+    expect(page).not_to have_css('a.btn', text: 'Pay $340.00 now')
+  end
+end
