e3d6fbff1f539a8799965f8e21a786e7600123c1
Camille Villa <cvilla100@gmail.com>
2019-08-05 10:02:49 -0700

    Extract payments history to its own feature test

--- spec/features/fines_spec.rb
+++ spec/features/fines_spec.rb
@@ -4,8 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'Fines Page', type: :feature do
   let(:user_with_payments) { '521181' }
-  let(:user_witout_fines) { '521206' }
-  let(:user_with_single_payment) { '521182' }
+  let(:user_without_fines) { '521206' }
 
   before do
     login_as(username: 'SUPER1', patron_key: user_with_payments)
@@ -49,92 +48,9 @@ RSpec.describe 'Fines Page', type: :feature do
     end
   end
 
-  context 'with multiple payments' do
-    it 'has a header for payment history' do
-      visit fines_path
-
-      expect(page).to have_css('h2', text: 'History')
-    end
-
-    it 'renders a list item for every payment', js: true do
-      visit fines_path
-      click_on 'Show history'
-
-      within('ul.payments') do
-        expect(page).to have_css('li', count: 2)
-        expect(page).to have_css('li h3', text: 'California : a history')
-        expect(page).to have_css('li .bill_description', text: 'Overdue item')
-      end
-    end
-
-    it 'has content behind a payments toggle', js: true do
-      visit fines_path
-      click_on 'Show history'
-
-      within('ul.payments') do
-        within(first('li')) do
-          expect(page).not_to have_css('dl', visible: true)
-          expect(page).not_to have_css('dt', text: 'Resolution', visible: true)
-          click_button 'Expand'
-          expect(page).to have_css('dl', visible: true)
-          expect(page).to have_css('dt', text: 'Resolution', visible: true)
-        end
-      end
-    end
-
-    it 'is sortable', js: true do
-      visit fines_path
-      click_on 'Show history'
-
-      within '#payments' do
-        expect(page).to have_css('.dropdown-toggle', text: 'Sort (Date paid)')
-        find('[data-sort="bill_description"]').click
-
-        expect(page).to have_css('.dropdown-toggle', text: 'Sort (Reason)')
-        expect(page).to have_css('.active[data-sort="bill_description"]', count: 2, visible: false)
-
-        within(first('ul.payments li')) do
-          expect(page).to have_css('.bill_description', text: /Overdue item/)
-        end
-      end
-    end
-  end
-
-  context 'when user has a single payment' do
-    before do
-      login_as(username: 'SUPER2', patron_key: user_with_single_payment)
-    end
-
-    it 'renders a list item for a single payment', js: true do
-      visit fines_path
-      click_on 'Show history'
-
-      within('ul.payments') do
-        expect(page).to have_css('li', count: 1)
-        expect(page).to have_css('li h3', text: 'No item associated with this payment')
-        expect(page).to have_css('li .bill_description', text: 'Privileges fee')
-      end
-    end
-
-    it 'has content behind a payments toggle', js: true do
-      visit fines_path
-      click_on 'Show history'
-
-      within('ul.payments') do
-        within(first('li')) do
-          expect(page).not_to have_css('dl', visible: true)
-          expect(page).not_to have_css('dt', text: 'Resolution', visible: true)
-          click_button 'Expand'
-          expect(page).to have_css('dl', visible: true)
-          expect(page).to have_css('dt', text: 'Resolution', visible: true)
-        end
-      end
-    end
-  end
-
   context 'with no fines' do
     before do
-      login_as(username: 'NOTHING', patron_key: user_witout_fines)
+      login_as(username: 'NOTHING', patron_key: user_without_fines)
     end
 
     it 'does not render table headers' do
