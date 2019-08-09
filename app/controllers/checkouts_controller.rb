48a41ae850d06500a7341ac46433d34310bae488
Chris Beer <cabeer@stanford.edu>
2019-08-09 16:17:01 -0700

    Add self-checkout

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -12,6 +12,24 @@ class CheckoutsController < ApplicationController
     @checkouts = patron_or_group.checkouts.sort_by { |x| x.sort_key(:due_date) }
   end
 
+  # GET /checkouts/new
+  def new; end
+
+  # POST /checkouts
+  def create
+    response = symphony_client.checkout(patron_or_group.barcode, params[:barcode])
+
+    case response.status
+    when 200
+      flash[:success] = t 'mylibrary.checkout.success_html' #, title: params['title']
+    else
+      Rails.logger.error(response.body)
+      flash[:error] = t 'mylibrary.checkout.error_html' # , title: params['title']
+    end
+
+    redirect_to checkouts_path(group: params[:group])
+  end
+
   private
 
   def item_details
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'sessions#index'
   resources :summaries
-  resources :checkouts
+  resources :checkouts, only: %I[index new create]
   resources :requests
   resources :fines
   resources :payments, only: :index
