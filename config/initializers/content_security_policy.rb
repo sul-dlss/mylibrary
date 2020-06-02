c125999e3fc2d5b2195c5a50855140c0df168ac0
Jack Reed <phillipjreed@gmail.com>
2020-06-02 07:25:12 -0600

    Tighten up CSP to enable unsafe inlines from oncehub

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -8,20 +8,22 @@
 
 # Commenting out while we need to have OnceHub/ScheduleOnce integration since they
 # write style and script tags into our page (effectively hosing any security CSP security we had)
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
-#
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self, :https
+  policy.font_src    :self, :https, :data
+  policy.img_src     :self, :https, :data
+  policy.object_src  :none
+  policy.script_src  :self, :https, :unsafe_inline
+  policy.style_src   :self, :https, :unsafe_inline
+
+  # Specify URI for violation reports
+  # policy.report_uri "/csp-violation-report-endpoint"
+end
 
 # If you are using UJS then enable automatic nonce generation
-Rails.application.config.content_security_policy_nonce_generator = ->(_request) { SecureRandom.base64(16) }
+# Note: Having the generator enabled, automatically adds nonce to the script-src policies which will ignore
+# specific domain inlines
+# Rails.application.config.content_security_policy_nonce_generator = ->(_request) { SecureRandom.base64(16) }
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
