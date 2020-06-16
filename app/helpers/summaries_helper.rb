fd5c90040e2bf277afc2635cf554c5edbdfdbf61
Jessie Keck <jessie.keck@gmail.com>
2020-06-16 12:42:45 -0700

    Add a link_to_schedule_once_visit helper method.

--- app/helpers/summaries_helper.rb
+++ app/helpers/summaries_helper.rb
@@ -7,4 +7,23 @@ module SummariesHelper
       a << [k, ERB::Util.url_encode(v)].join('=')
     end.join('&')
   end
+
+  def link_to_schedule_once_visit(library:, text:, css_class: nil)
+    link_to(
+      text,
+      library_schedule_path_map[library],
+      role: 'button',
+      class: css_class,
+      data: { 'mylibrary-modal': 'trigger' }
+    )
+  end
+
+  private
+
+  def library_schedule_path_map
+    {
+      'EAST-ASIA' => schedule_eal_path,
+      'GREEN' => schedule_green_path
+    }
+  end
 end
--- spec/helpers/summaries_helper_spec.rb
+++ spec/helpers/summaries_helper_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe SummariesHelper do
       ).to eq 'blah=Jane%20Stanford&other_blah=Hello%40example.com'
     end
   end
+
+  describe '#link_to_schedule_once_visit' do
+    it 'is a link with the appropriate data attributes' do
+      link = link_to_schedule_once_visit(library: 'GREEN', text: 'Green Library')
+      expect(link).to include 'data-mylibrary-modal="trigger"'
+    end
+
+    it 'links to the appropriate href given the library' do
+      link = link_to_schedule_once_visit(library: 'EAST-ASIA', text: 'East Asia Library')
+      expect(link).to include 'href="/schedule/eal"'
+    end
+
+    it 'uses the given text as the link text' do
+      link = Capybara.string(link_to_schedule_once_visit(library: 'GREEN', text: 'Green Library'))
+      expect(link).to have_link('Green Library', href: '/schedule/green')
+    end
+  end
 end
