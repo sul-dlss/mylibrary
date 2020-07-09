9f04c3aaf39cac4adc419852e8a57bff895e53b2
Jessie Keck <jessie.keck@gmail.com>
2020-07-09 11:52:46 -0700

    Move button/link to schedule pickups to a helper method.

--- app/helpers/summaries_helper.rb
+++ app/helpers/summaries_helper.rb
@@ -34,12 +34,32 @@ module SummariesHelper
     render 'schedules/schedule_library_visit_dropdown', schedulable_libraries: schedulable_libraries
   end
 
+  def schedule_pickup_link_or_dropdown
+    pickup_libraries = library_pickup_path_map.keys
+    if pickup_libraries.one?
+      return link_to(
+        "🗓 Schedule pickup at #{library_name(pickup_libraries.first)}",
+        library_pickup_path_map[pickup_libraries.first],
+        role: 'button',
+        class: 'btn btn-primary',
+          data: { 'mylibrary-modal': 'trigger' }
+        )
+    end
+  end
+
   private
 
   def library_schedule_path_map
     map = {}
     map['GREEN'] = schedule_green_path if patron_or_group.can_schedule_green_access?
     map['EAST-ASIA'] = schedule_eal_path if patron_or_group.can_schedule_eal_access?
+
+    map
+  end
+
+  def library_pickup_path_map
+    map = {}
+    map['GREEN'] = schedule_pickup_path if patron_or_group.can_schedule_green_pickup?
 
     map
   end
