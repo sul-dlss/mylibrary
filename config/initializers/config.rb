a6fb8db0d78b345f32188c4972b768ccee2a2a80
Nick Budak <budak@stanford.edu>
2023-08-03 14:09:39 -0700

    Read config values from the environment
    Needed to pick up e.g. https://github.com/sul-dlss/puppet/pull/9737

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -3,4 +3,9 @@
 Config.setup do |config|
   # Name of the constant exposing loaded settings
   config.const_name = 'Settings'
+  # Load values from the environment; SETTINGS__FOO__BAR_BAZ => Settings.foo.bar_baz
+  config.use_env = true
+  config.env_prefix = 'SETTINGS'
+  config.env_separator = '__'
+  config.env_converter = :downcase
 end
