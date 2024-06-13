e80bcf96bcdd36bc634745295e80825abae29a2a
Cory Lown <corylown@stanford.edu>
2024-06-13 15:40:09 -0400

    Switch to jsbundling-rails and esbuild for javascript

---
+++ app/javascript/application.js
@@ -0,0 +1,19 @@
+// Entry point for the build script in your package.json
+
+import './jquery-shim'
+
+import Rails from 'rails-ujs'
+import Turbolinks from 'turbolinks'
+
+import './ajax_in_place_updates'
+import './analytics'
+import './contact_form'
+import './convertButton'
+import './listSort'
+import './modal'
+import './nav_spinners'
+import './showPassword'
+import './toggleClassWithExpand'
+
+Rails.start()
+Turbolinks.start()
