332779f284695103be5f30290d09499660f477ca
Chris Beer <cabeer@stanford.edu>
2026-05-11 11:46:37 -0700

    Consolidate RenewalsController behavior into CheckoutsController

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -2,14 +2,96 @@
 
 # Controller for the Checkouts page
 class CheckoutsController < ApplicationController
+  include ActionView::Context
+  include ActionView::Helpers::TagHelper
+
   before_action :authenticate_user!
+
+  before_action :load_checkouts
+  before_action :load_checkout, except: %i[index renew_eligible]
+
+  before_action :authorize_renew!, only: [:renew]
+
+  rescue_from CheckoutException, with: :deny_access
 
   # Render a list of checkouts for the patron or research group
   #
   # GET /checkouts
   # GET /checkouts.json
   def index
-    @checkouts = patron_or_group.checkouts.sort_by { |x| x.sort_key(:due_date) }
     @requests = patron_or_group.requests.sort_by { |request| request.sort_key(:date) }
+  end
+
+  # Renew a single item for a patron
+  #
+  # POST /checkouts/:id/renew
+  def renew # rubocop:disable Metrics/AbcSize
+    @response = ils_client.renew_item_by_id(patron_or_group.key, checkout_id_param)
+
+    case @response.status
+    when 200, 201
+      flash[:success] = t 'mylibrary.renew_item.success_html', title: params['title']
+    else
+      flash[:error] = t 'mylibrary.renew_item.error_html', title: params['title']
+    end
+
+    redirect_to checkouts_path(group: params[:group])
+  end
+
+  # Renew all eligible items for a patron
+  #
+  # POST /checkouts/renew_eligible
+  def renew_eligible
+    eligible_renewals = @checkouts.select(&:renewable?)
+    response = ils_client.renew_items(eligible_renewals)
+
+    bulk_renewal_success_flash(response)
+    bulk_renewal_error_flash(response)
+
+    redirect_to checkouts_path(group: params[:group])
+  end
+
+  private
+
+  def load_checkouts
+    @checkouts = patron_or_group.checkouts.sort_by { |x| x.sort_key(:due_date) }
+  end
+
+  def load_checkout
+    @checkout = @checkouts.find { |checkout| checkout.item_id == checkout_id_param }
+
+    raise CheckoutException, 'Checkout not found' if @checkout.nil?
+  end
+
+  def checkout_id_param
+    params.require(:id)
+  end
+
+  # Make sure the checkout belongs to the user trying to do the renewal
+  # and make sure the item is renewable
+  def authorize_renew!
+    raise CheckoutException, 'Error' if @checkout.item_category_non_renewable?
+  end
+
+  def bulk_renewal_success_flash(response)
+    return unless response[:success].any?
+
+    flash[:success] = I18n.t('mylibrary.renew_all_items.success_html', count: response[:success].length)
+  end
+
+  def bulk_renewal_error_flash(response)
+    return unless response[:error].any?
+
+    flash[:error] = I18n.t('mylibrary.renew_all_items.error_html',
+                           count: response[:error].length,
+                           items: tag.ul(safe_join(response[:error].collect do |renewal|
+                                                     tag.li(renewal.title.truncate_words(7))
+                                                   end, '')))
+  end
+
+  def deny_access
+    flash[:error] = 'An unexpected error has occurred' # rubocop:disable Rails/I18nLocaleTexts
+
+    redirect_to checkouts_path(group: params[:group])
   end
 end
--- spec/controllers/checkouts_controller_spec.rb
+++ spec/controllers/checkouts_controller_spec.rb
@@ -3,37 +3,35 @@
 require 'rails_helper'
 
 RSpec.describe CheckoutsController do
-  let(:mock_patron) { instance_double(Folio::Patron) }
+  let(:mock_patron) { instance_double(Folio::Patron, checkouts: checkouts, requests: requests, key: '513a9054-5897-11ee-8c99-0242ac120002') }
 
   let(:mock_client) { instance_double(FolioClient, ping: true) }
   let(:requests) { [] }
+  let(:checkouts) { [] }
+
+  let(:user) do
+    User.new(username: 'somesunetid', patron_key: '513a9054-5897-11ee-8c99-0242ac120002')
+  end
 
   before do
     allow(FolioClient).to receive(:new).and_return(mock_client)
     allow(controller).to receive(:patron).and_return(mock_patron)
-    allow(mock_patron).to receive(:requests).and_return(requests)
+    warden.set_user(user) if user
   end
 
   context 'with an unauthenticated request' do
+    let(:user) { nil }
+
     it 'redirects to the home page' do
       expect(get(:index)).to redirect_to root_url
     end
   end
 
   context 'with an authenticated request' do
-    let(:user) do
-      User.new(username: 'somesunetid', patron_key: '513a9054-5897-11ee-8c99-0242ac120002')
-    end
-
     let(:checkouts) do
       [
         instance_double(Folio::Checkout, key: '1', sort_key: nil)
       ]
-    end
-
-    before do
-      allow(mock_patron).to receive(:checkouts).and_return(checkouts)
-      warden.set_user(user)
     end
 
     it 'displays list of checkouts' do
@@ -62,10 +60,6 @@ RSpec.describe CheckoutsController do
   end
 
   context 'with an authenticated request for group checkouts' do
-    let(:user) do
-      User.new(username: 'somesunetid', patron_key: '513a9054-5897-11ee-8c99-0242ac120002')
-    end
-
     let(:checkouts) do
       [
         instance_double(Folio::Checkout, key: '2', sort_key: nil)
@@ -75,13 +69,142 @@ RSpec.describe CheckoutsController do
     before do
       allow(mock_patron).to receive(:proxy_group).and_return(instance_double(Folio::Group, checkouts:,
                                                                                            requests:))
-      warden.set_user(user)
     end
 
     it 'assigns a list of checkouts' do
       get(:index, params: { group: true })
 
       expect(assigns(:checkouts)).to eq checkouts
+    end
+  end
+
+  describe '#renew' do
+    let(:api_response) { instance_double(Faraday::Response, status: 201) }
+    let(:checkouts) { [instance_double(Folio::Checkout, item_id: '123', item_category_non_renewable?: false, sort_key: nil)] }
+
+    before do
+      allow(mock_client).to receive(:renew_item_by_id).and_return(api_response)
+      allow(FolioClient).to receive(:new).and_return(mock_client)
+      allow(controller).to receive(:patron_or_group).and_return(mock_patron)
+    end
+
+    context 'when everything is good' do
+      it 'renews the item and sets flash messages' do
+        post :renew, params: { id: '123' }
+
+        expect(flash[:success]).to match(/Success!/)
+      end
+
+      it 'renews the item and redirects to checkouts_path' do
+        post :renew, params: { id: '123' }
+
+        expect(response).to redirect_to checkouts_path
+      end
+    end
+
+    context 'when the response is not 201' do
+      let(:api_response) { instance_double(Faraday::Response, status: 401) }
+
+      it 'does not renew the item and sets flash messages' do
+        post :renew, params: { id: '123' }
+
+        expect(flash[:error]).to match(/Sorry!/)
+      end
+
+      it 'does not renew the item and redirects to checkouts_path' do
+        post :renew, params: { id: '123' }
+
+        expect(response).to redirect_to checkouts_path
+      end
+    end
+
+    context 'with a group request' do
+      it 'renews the item and redirects to checkouts_path' do
+        post :renew, params: { id: '123', group: true }
+
+        expect(response).to redirect_to checkouts_path(group: true)
+      end
+    end
+
+    context 'when the requested item is not checked out to the patron' do
+      it 'does not renew the item and sets flash messages' do
+        post :renew, params: { id: 'some_made_up_item_id' }
+
+        expect(flash[:error]).to match('An unexpected error has occurred')
+      end
+
+      it 'does not renew the item and redirects to checkouts_path' do
+        post :renew, params: { id: 'some_made_up_item_id' }
+
+        expect(response).to redirect_to checkouts_path
+      end
+    end
+
+    context 'when the requested item is not eligible even though Folio does not stop us' do
+      let(:checkouts) { [instance_double(Folio::Checkout, item_id: '123', sort_key: nil, item_category_non_renewable?: true)] }
+
+      it 'does not renew the item and sets flash messages' do
+        post :renew, params: { id: '123' }
+
+        expect(flash[:error]).to match('An unexpected error has occurred')
+      end
+    end
+  end
+
+  describe '#renew_eligible' do
+    let(:api_response) { { success: [checkouts[0]], error: [checkouts[1]] } }
+    let(:checkouts) do
+      [
+        instance_double(Folio::Checkout, key: '1', renewable?: true, item_id: '123', title: 'ABC', sort_key: nil),
+        instance_double(Folio::Checkout, key: '2', renewable?: true, item_id: '456', sort_key: nil,
+                                         title: 'Principles of optics : electromagnetic theory of ' \
+                                                'propagation, interference and diffraction of light'),
+        instance_double(Folio::Checkout, key: '3', renewable?: false, item_id: '789', sort_key: nil, title: 'Not')
+      ]
+    end
+
+    before do
+      allow(mock_client).to receive(:renew_items).and_return(api_response)
+    end
+
+    it 'sends renewal requests to Folio for eligible items' do
+      post :renew_eligible
+
+      expect(mock_client).to have_received(:renew_items).with([
+                                                                having_attributes(key: '1'),
+                                                                having_attributes(key: '2')
+                                                              ])
+    end
+
+    context 'when successful' do
+      before { post :renew_eligible }
+
+      it 'sets a success flash message' do
+        expect(flash[:success]).to include('Success!')
+      end
+
+      it 'includes the number of renewed items' do
+        expect(flash[:success]).to include('1 item was renewed')
+      end
+    end
+
+    describe 'when unsuccessful' do
+      before { post :renew_eligible }
+
+      it 'sets an error flash message' do
+        expect(flash[:error]).to include('Sorry!')
+      end
+
+      it 'includes the truncated titles of errored renewals' do
+        expect(Capybara.string(flash[:error])).to have_css('li',
+                                                           text: 'Principles of optics : electromagnetic theory of...')
+      end
+    end
+
+    it 'renews the item and redirects to checkouts_path' do
+      post :renew_eligible
+
+      expect(response).to redirect_to checkouts_path
     end
   end
 end
